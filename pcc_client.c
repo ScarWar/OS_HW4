9326531469169d2075e6cc17a9f632777bfbedba
ScarWar <ariael2009@gmail.com>
2017-06-19 19:30:45 +0300

    New functions

--- pcc_client.c
+++ pcc_client.c
@@ -15,145 +15,145 @@
 #define BUFFER_SIZE 2048
 #define NUM_OF_PRINTABLE_CHAR 95
 #define PORT_NUM 2233
-#define min(s,t) ((s) < (t) ? (s) : (t))
+#define min(s, t) ((s) < (t) ? (s) : (t))
 
 typedef struct statistics_data_t {
-	long long char_statistics[NUM_OF_PRINTABLE_CHAR] = {0};
+    long long char_statistics[NUM_OF_PRINTABLE_CHAR];
 } StatisticsData;
 
 
-int main(int argc, char const *argv[])
-{
-	int 	LEN, inputfd, sockfd, total_sent;
-	ssize_t read_num, read_buffer_size, n_bytes_sent,
-			n_bytes_recv;
-	char 	*output_buffer = NULL,
-			*input_buffer  = NULL;
+int main(int argc, char const *argv[]) {
+    int inputfd, sockfd, total_sent;
+    long LEN;
+    ssize_t n_bytes_read, read_buffer_size, n_bytes_sent,
+            n_bytes_recv;
+    char *output_buffer = NULL,
+            *input_buffer = NULL;
 
-	struct 	sockaddr_in dest_addr = {0};
+    struct sockaddr_in dest_addr = {0};
 
-	if(argc != 2 ){
-		printf("[Error] - %s\n", "Invalide number of arguments, expected 2");
-		return -1;
-	}
-
-	/* Read the length (in bytes) of the stream to process */
-	LEN = strtol(argv[2], NULL, 10);
-	if(ERANGE != errno){
-		report_error("Unable to convert string to integer");
-		return -1;
-	}
-	
-	/* Open a socket to Server on your local machine, port 2233 */
-	if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0){
-		report_error("Socket creation failed");
-		return -1;
-	}
-
-	/* Set parameters of socket connection */
-	dest_addr.sin_family = AF_INET;
-	dest_addr.sin_port	 = htons(2233);
-	dest_addr.sin_addr   = htonl(inet_pton("127.0.0.1"));
-
-	/* Connect to Server */
-	if (connect(sockfd, (struct sockaddr *) &dest_addr, sizeof(dest_addr)) < 0){
-		report_error("Failed to connect to server");
-		return -1;
-	}
-
-	/* Open /dev/urandom for reading */
-	if((inputfd = open(DEFAULT_SOURCE, O_RDONLY)) < 0){
-		report_error("Failed to open input file");
-		if (close(sockfd) < 0)
-	        report_error("Failed to close sokcet");
-		return -1;
-	}
-
-	/* Transfer (write) LEN bytes from /dev/urandom to Server through the socket.
-	 * Send LEN to Server
-	 */
-	total_sent = 0;
-	while(total_sent < sizeof(int)){
-		n_bytes_sent = send(sockfd, &LEN + total_sent, sizeof(int) - total_sent, 0);
-		if(n_bytes_sent < 0){
-			report_error("Failed to send LEN to server");
-		    if (close(sockfd) < 0)
-		        report_error("Failed to close sokcet");
-			if(close(inputfd) < 0)
-        		report_error("Failed to close data stream");
-			return -1;
-		}
-		total_sent += n_bytes_sent;
-	}
-
-	output_buffer = (char *) malloc(BUFFER_SIZE);
-	if(!output_buffer){
-		report_error("Memory allocation failed");
-	    if (close(sockfd) < 0)
-	        report_error("Failed to close sokcet");
-		if(close(inputfd) < 0)
-    		report_error("Failed to close data stream");
-		return -1;
-	}
-
-	/* Read from input than send to server */
-	do {
-		total_sent = 0;
-		read_buffer_size = (ssize_t) min(LEN, BUFFER_SIZE);
-		read_num = read(inputfd, output_buffer, read_buffer_size);
-
-		/* If read_num < 0 handle error */
-		if(read_num < 0){
-			report_error("Failed to read from input file");
-	    	if (close(sockfd) < 0)
-	        	report_error("Failed to close sokcet");
-			if(close(inputfd) < 0)
-    			report_error("Failed to close data stream");
-			free(output_buffer);
-			return -1;
-		}
-
-		/* Send buffer to server */
-		while(read_num > 0){
-			n_bytes_sent = send(
-				sockfd, 
-				output_buffer + total_sent, 
-				read_num, 0);
-
-			if(n_bytes_sent < 0){
-				report_error("Failed to send to server");
-	    		if (close(sockfd) < 0)
-	        		report_error("Failed to close sokcet");			
-				if(close(inputfd) < 0)
-    				report_error("Failed to close data stream");
-				free(output_buffer);
-				return -1;
-			}
-			/* Update number of bytes left to send*/
-			read_num -= n_bytes_sent;
-			total_sent += n_bytes_sent;
-		}
-
-		/* Update number of bytes left to send*/
-		LEN -= read_num;
-	} while(LEN > 0);
-
-	/* Get (read) the result from Server through the socket */
-
-	/* Print the result */
-
-	/* Close descriptors */
-	if(close(inputfd) < 0){
-        report_error("Failed to close input file");
-        return -1;
-	}
-
-    /* Close socket*/
-    if (close(sockfd) < 0){
-        report_error("Failed to close sokcet");
+    if (argc != 2) {
+        printf("[Error] - %s\n", "Invalide number of arguments, expected 2");
         return -1;
     }
 
-	/* Quit */
-	return 0;
+    /* Read the length (in bytes) of the stream to process */
+    LEN = strtol(argv[2], NULL, 10);
+    if (ERANGE != errno) {
+        report_error("Unable to convert string to integer");
+        return -1;
+    }
+
+    /* Open a socket to Server on your local machine, port 2233 */
+    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        report_error("Socket creation failed");
+        return -1;
+    }
+
+    /* Set parameters of socket connection */
+    dest_addr.sin_family = AF_INET;
+    dest_addr.sin_port = htons(PORT_NUM);
+    dest_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+    /* Connect to Server */
+    if (connect(sockfd, (struct sockaddr *) &dest_addr, sizeof(dest_addr)) < 0) {
+        report_error("Failed to connect to server");
+        return -1;
+    }
+
+    /* Open /dev/urandom for reading */
+    if ((inputfd = open(DEFAULT_SOURCE, O_RDONLY)) < 0) {
+        report_error("Failed to open input file");
+        if (close(sockfd) < 0)
+            report_error("Failed to close socket");
+        return -1;
+    }
+
+    /* Transfer (write) LEN bytes from /dev/urandom to Server through the socket.
+     * Send LEN to Server
+     */
+    total_sent = 0;
+    while (total_sent < sizeof(int)) {
+        n_bytes_sent = send(sockfd, &LEN + total_sent, sizeof(int) - total_sent, 0);
+        if (n_bytes_sent < 0) {
+            report_error("Failed to send LEN to server");
+            if (close(sockfd) < 0)
+                report_error("Failed to close socket");
+            if (close(inputfd) < 0)
+                report_error("Failed to close data stream");
+            return -1;
+        }
+        total_sent += n_bytes_sent;
+    }
+
+    output_buffer = (char *) malloc(BUFFER_SIZE);
+    if (!output_buffer) {
+        report_error("Memory allocation failed");
+        if (close(sockfd) < 0)
+            report_error("Failed to close socket");
+        if (close(inputfd) < 0)
+            report_error("Failed to close data stream");
+        return -1;
+    }
+
+    /* Read from input than send to server */
+    while (LEN < 0) {
+        total_sent = 0;
+        read_buffer_size = (ssize_t) min(LEN, BUFFER_SIZE);
+        n_bytes_read = read(inputfd, output_buffer, (size_t) read_buffer_size);
+
+        /* If n_bytes_read < 0 handle error */
+        if (n_bytes_read < 0) {
+            report_error("Failed to read from input file");
+            if (close(sockfd) < 0)
+                report_error("Failed to close socket");
+            if (close(inputfd) < 0)
+                report_error("Failed to close data stream");
+            free(output_buffer);
+            return -1;
+        }
+
+        /* Send buffer to server */
+        while (n_bytes_read > 0) {
+            n_bytes_sent = send(
+                    sockfd,                     // Socket file descriptor
+                    output_buffer + total_sent, // Offset
+                    (size_t) n_bytes_read, 0);  // number of bytes left to send
+
+            if (n_bytes_sent < 0) {
+                report_error("Failed to send to server");
+                if (close(sockfd) < 0)
+                    report_error("Failed to close socket");
+                if (close(inputfd) < 0)
+                    report_error("Failed to close data stream");
+                free(output_buffer);
+                return -1;
+            }
+            /* Update number of bytes left to send */
+            n_bytes_read -= n_bytes_sent;
+            total_sent += n_bytes_sent;
+        }
+
+        /* Update number of bytes left to send*/
+        LEN -= n_bytes_read;
+    }
+
+    /* Get (read) the result from Server through the socket */
+
+    /* Print the result */
+
+    /* Close descriptors */
+    if (close(inputfd) < 0) {
+        report_error("Failed to close input file");
+        return -1;
+    }
+
+    /* Close socket*/
+    if (close(sockfd) < 0) {
+        report_error("Failed to close socket");
+        return -1;
+    }
+
+    /* Quit */
+    return 0;
 }
--- pcc_server.c
+++ pcc_server.c
@@ -9,16 +9,16 @@
 #include <arpa/inet.h>
 #include <errno.h>
 #include <pthread.h>
-
+#include <signal.h>
 
 #define report_error(str) printf("LINE: %d, Error - %s %s\n", __LINE__, (str), strerror(errno))
 #define BUFFER_SIZE 2048
 #define NUM_OF_PRINTABLE_CHAR 95
 #define PORT_NUM 2233
-#define min(s,t) ((s) < (t) ? (s) : (t))
+#define min(s, t) ((s) < (t) ? (s) : (t))
 
 typedef struct statistics_data_t {
-	long long char_statistics[NUM_OF_PRINTABLE_CHAR] = {0};
+    long long char_statistics[NUM_OF_PRINTABLE_CHAR] = {0};
 } StatisticsData;
 
 static StatisticsData global_sd;
@@ -26,104 +26,145 @@ static long long global_n_bytes_read = 0;
 pthread_mutex_t lock;
 
 
-
-int is_printable(char c) {
-    return 32 <= c && c <= 126;
-}
-
-
 void my_signal_handler(int signum, siginfo_t *info, void *ptr) {
     /* Calculate pipe name */
     pid_t child_pid = info->si_pid;
 
     /* Stop listening the port. Close the socket. */
-	/* Wait for all running threads to finish. */
-	/* Print the global bytes counter. */
-	printf("The number of bytes read by server is %lld\n",global_n_bytes_read);
-	/* Print the global statistics. */
+    /* Wait for all running threads to finish. */
+    /* Print the global bytes counter. */
+    printf("The number of bytes read by server is %lld\n", global_n_bytes_read);
+    /* Print the global statistics. */
 
-	/* Exit process. */
+    /* Exit process. */
 
 
 }
 
-void *start_client_thread(void *arg){
-	long long 	n_bytes_read = 0, n_bytes_recv = 0,
-				LEN, total_read;
-	int 		i;
-	char 		*buf = NULL;
-	/* Create a statistics local data structure. */
-	StatisticsData sd = (StatisticsData*) malloc(sizeof(StatisticsData));
-	if(!sd){
-		report_error("Memory allocation failed");
-		pthread_exit(NULL);
-	}
-	int sockfd = *((int *) arg);
+int is_printable(char c) {
+    return 32 <= c && c <= 126;
+}
 
-	/*
-	Read the content from the socket. For every byte:
-	 - Increment the number of bytes read.
-	 - Decide whether it is printable or not.
-	 - If it is, then update the local statistics.
-	*/
-	while(total_read < sizeof(int)){
-		n_bytes_recv = recv(sockfd, &LEN + total_sent, sizeof(int) - total_sent, 0);
-		if(n_bytes_recv < 0){
-			report_error("Failed to read from client");
-			free(sd);
-			pthread_exit(NULL);
-		}
-		total_read += n_bytes_recv;
-	}
+int count_printable(char *buf, int length, StatisticsData *sd) {
+    int i;
+    char c;
+    if (buf == NULL || sd == NULL) {
+        report_error("Null pointer argument");
+        return -1;
+    } else if (length < 0) {
+        report_error("Invalid argument");
+        return -1;
+    }
 
-	n_bytes_read = 0;
-	buf = (char *) malloc(BUFFER_SIZE);
-	if(!buf){
-		report_error("Memory allocation failed")
-		free(sd);
-		pthread_exit(NULL);
-	}
-	while (n_bytes_read < LEN){
-		n_bytes_recv = recv(sockfd, buf, LEN - n_bytes_read, 0);
-		if(n_bytes_recv < 0){
-			report_error("Failed to read from socket");
-			free(sd);
-			free(buf);
-			pthread_exit(NULL);
-		}
-	}
+    for (i = 0; i < length; ++i) {
+        c = buf[i];
+        if (is_printable(c))
+            sd->char_statistics[c - 32]++;
+    }
+    return 0;
 
- 	/* Send the number of the printable bytes back to the client. */
-
-	/* Close the connection. */
-
-	/* Acquire (lock) mutex. */
-	if(!pthread_mutex_lock(&lock)){
-		// TODO
-		break;
-	}
-
-	/* Add local statistics to GLOB STATS. */
-	for(i = 0; i < NUM_OF_PRINTABLE_CHAR; ++i)
-		global_sd->char_statistics[i] += sd->char_statistics[i]
-
-	/* Add the number of bytes read by this thread to the global counter. */
-
-	/* Release (unlock) mutex. */
-	if(!pthread_mutex_unlock(&lock)){
-		// TODO 
-		break;
-	}
-	/* Exit thread. */
-	pthread_exit(NULL);
 }
 
 
-int main(int argc, char const *argv[])
-{
+void *start_client_thread(void *arg) {
+    long long n_bytes_read = 0, n_bytes_recv = 0,
+            LEN, total_read, n_printable_bytes,
+            total_sent, n_bytes_sent, n_bytes_to_read;
+    int i;
+    char *buf = NULL, print_buf = NULL;
+    /* Create a statistics local data structure. */
+    StatisticsData *sd = (StatisticsData *) malloc(sizeof(StatisticsData));
+    if (!sd) {
+        report_error("Memory allocation failed");
+        pthread_exit(NULL);
+    }
+    int sockfd = *((int *) arg);
+
+    /*
+    Read the content from the socket. For every byte:
+        - Increment the number of bytes read.
+        - Decide whether it is printable or not.
+        - If it is, then update the local statistics.
+    */
+    while (total_read < sizeof(int)) {
+        n_bytes_recv = recv(sockfd, &LEN + total_sent, sizeof(int) - total_sent, 0);
+        if (n_bytes_recv < 0) {
+            report_error("Failed to read from client");
+            free(sd);
+            pthread_exit(NULL);
+        }
+        total_read += n_bytes_recv;
+    }
+
+    n_bytes_read = 0;
+    buf = (char *) malloc(BUFFER_SIZE);
+    if (!buf) {
+        report_error("Memory allocation failed");
+        free(sd);
+        free(buf);
+        pthread_exit(NULL);
+    }
+    while (n_bytes_read < LEN) {
+        n_bytes_to_read = min(LEN - n_bytes_read, BUFFER_SIZE);
+        n_bytes_recv = recv(sockfd, buf, (size_t) (LEN - n_bytes_read), 0);
+        if (n_bytes_recv < 0) {
+            report_error("Failed to read from socket");
+            free(sd);
+            free(buf);
+            pthread_exit(NULL);
+        }
+        if (!count_printable(buf, (int) n_bytes_recv, sd)) {
+            // TODO error - handle thread error
+        }
+        n_bytes_read += n_bytes_recv;
+    }
+
+    /* Send the number of the printable bytes back to the client. */
+    n_printable_bytes = 0;
+    for (i = 0; i < NUM_OF_PRINTABLE_CHAR; ++i)
+        n_printable_bytes += sd->char_statistics[i];
+
+    total_sent = 0;
+    while (total_sent < sizeof(int)) {
+        n_bytes_sent = send(sockfd, &n_printable_bytes + total_sent, sizeof(long long) - total_sent, 0);
+        if (n_bytes_sent < 0) {
+            report_error("Failed to send to server");
+            if (close(sockfd) < 0)
+                report_error("Failed to close socket");
+            pthread_exit(NULL);
+        }
+        total_sent += n_bytes_sent;
+    }
+
+    /* Close the connection. */
+    close(sockfd);
+
+    /* Acquire (lock) mutex. */
+    if (!pthread_mutex_lock(&lock)) {
+        // TODO
+    }
+
+    /* Add local statistics to GLOB STATS. */
+    for (i = 0; i < NUM_OF_PRINTABLE_CHAR; ++i)
+        global_sd->char_statistics[i] += sd->char_statistics[i]
+
+    /* Add the number of bytes read by this thread to the global counter. */
+    global_n_bytes_read += n_bytes_read;
+
+    /* Release (unlock) mutex. */
+    if (!pthread_mutex_unlock(&lock)) {
+        // TODO 
+        break;
+    }
+    /* Exit thread. */
+    pthread_exit(NULL);
+}
 
 
-	/* Structure to pass to the registration syscall */
+int main(int argc, char const *argv[]) {
+
+
+    /* Structure to pass to the registration syscall */
     struct sigaction new_action;
     memset(&new_action, 0, sizeof(new_action));
     /* Assign pointer to our handler function */
@@ -132,9 +173,9 @@ int main(int argc, char const *argv[])
     new_action.sa_flags = SIGINT;
 
     if (0 != sigaction(SIGUSR1, &new_action, NULL)) {
-        ERROR_HANDLE("Signal handle registration failed");
+        report_error("Signal handle registration failed");
         return -1;
     }
 
-	return 0;
+    return 0;
 }
